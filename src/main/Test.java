b75112974dbd12516bd2fd0938c335ce08031f12
ro-per <romeo.permentier@gmai.com>
2020-04-08 00:28:25 +0200

    update MVC

--- src/main/Test.java
+++ src/main/Test.java
@@ -17,15 +17,17 @@ public class Test {
     private static int timer;
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-        init();
+        init("30_3");
+        //init("20000_4");
+        //init("20000_20");
+
         run();
     }
 
-    public static void init() throws IOException, SAXException, ParserConfigurationException {
+    public static void init(String amount) throws IOException, SAXException, ParserConfigurationException {
          ram = new Ram();
          InstructionReader instructionReader = InstructionReader.getInstance();
-         String filename = InstructionReader.INSTR_30_4;
-         instructionList = instructionReader.readInstructions(filename);
+         instructionList = instructionReader.readInstructions(amount);
          timer = 0;
     }
 
@@ -37,25 +39,25 @@ public class Test {
                 case "Read":
                     System.out.println("Reading process " + currentInstruction.getProcessID() + " with virtual address "+ currentInstruction.getVirtualAddress());
                     ram.read(currentInstruction.getProcessID(),currentInstruction.getPageNumber(),timer);
-                    System.out.println("Readed "+ ram.toString());
+                    System.out.println("Readed "+ ram);
                     break;
                 case "Write":
                     System.out.println("Writing to process " + currentInstruction.getProcessID() + " with virtual address "+ currentInstruction.getVirtualAddress());
                     ram.write(currentInstruction.getProcessID(),currentInstruction.getPageNumber(),timer);
-                    System.out.println("Writed " + ram.toString());
+                    System.out.println("Writed " + ram);
                     break;
                 case "Start":
                     System.out.println("Starting process " + currentInstruction.getProcessID());
                     Process currentProcess = new Process(currentInstruction.getProcessID());
                     ram.addProcess(currentProcess);
                     ram.adjustFrames();
-                    System.out.println("Started: " + ram.toString());
+                    System.out.println("Started: " + ram);
                     break;
                 case "Terminate":
                     System.out.println("Terminating process " + currentInstruction.getProcessID());
                     ram.removeProcess(currentInstruction.getProcessID());
                     ram.adjustFrames();
-                    System.out.println("Terminated:" + ram.toString());
+                    System.out.println("Terminated:" + ram);
                     break;
                 default:
                     break;
